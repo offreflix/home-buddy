82aebd127a1b60ce04c1da9b97a9b8c964d57d64
João Victor <offreflix@gmail.com>
2025-03-22 21:33:36 -0300

    fix: adjust existing user error

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
@@ -28,7 +32,7 @@ export class AuthService {
     );
 
     if (existingUsername || existingEmail) {
-      throw new UnauthorizedException('User already exists');
+      throw new ConflictException('User already exists');
     }
 
     const user = await this.usersService.createUser({
